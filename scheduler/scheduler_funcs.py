01eba56177d9fb91388aaca8a1b69ecc9cebb611
qaser <huji@mail.ru>
2022-11-08 14:54:46 +0500

    добавлена рассылка

--- scheduler/scheduler_funcs.py
+++ scheduler/scheduler_funcs.py
@@ -6,7 +6,7 @@ def send_remainder():
     queryset = list(groups.find({'sub_banned': 'false'}))
     for group in queryset:
         id = group.get('_id')
-        print(type(id))
+        print(int(id))
         # await bot.send_message(
         #     chat_id=group.get('_id')
         # )
