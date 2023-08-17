fc18f094dba744c164309cab671fe3181e831cd0
Alexey S <huji@mail.ru>
2023-08-17 14:29:38 +0500

    add stats

---
+++ handlers/stats.py
@@ -0,0 +1,26 @@
+from aiogram import types
+
+from config.bot_config import dp
+from config.mongo_config import emergency_stops
+from utils.decorators import admin_check
+
+
+@admin_check
+@dp.message_handler(commands=['stats'])
+async def stats_handler(message: types.Message):
+    if message.chat.type == 'private':
+        res_text = ''
+        count_ao = emergency_stops.count_documents()
+        pipeline = [
+            {'$group': {'_id': '$station', 'count': {'$sum': 1}}},
+            {'$sort': { 'station': 1}}
+        ]
+        queryset = list(emergency_stops.aggregate(pipeline))
+        for item in queryset:
+            name = item.get('_id')
+            count = item.get('count')
+            station_text = f'{name}: {count}\n'
+            res_text = f'{res_text}{station_text}'
+        summary_text = f'Всего ботом учтено {count_ao} АО(ВНО)\n\n{res_text}'
+        await message.delete()
+        await message.answer(summary_text)
--- utils/test.py
+++ utils/test.py
@@ -1,28 +1,24 @@
-import datetime as dt
+import re
 
-import pymongo
+GROUP_NAMES = [
+    "Верхнеказымское ЛПУМГ АО ГПА-Ц-16 ст. №93 29.03.2023",
+    "Лялинская КС ст. №63, ГТК-25ИР, АО, 16.03.2023",
+    "КС Ямбургская - 11 ГПА-Ц-16 АО 17.04.2023"
+    "Пуровская-15 ГТК-10И АО 24.01.2023",
+    "Приозерная-51 ГПА-Ц-16 АО 21.01.2023",
+    "Пелымская-63 ГТК-25ИР АО 27.12.2022",
+    "КС Краснотурьинская АО ГПА 91 ГТК-25И 03.12.22",
+    "Бобровская-52 ГПА-Ц-16 АО 29.11.2022",
+    "Сосновское ЛПУМГ- ГПА-Ц-16 ст.N 104 АО 16.11.2023",
+    "КС Уренгойская-34 ГПА-12Р Урал, АО 15.11.2022",
+    "КС Лялинская ГТК-25И(Р) ст.N33 АО 08.08.2023 23:20"
+]
 
-# Create the client
-client = pymongo.MongoClient('localhost', 27017)
-db = client['poks_bot_db']
-petitions = db['petitions']
+date_reg = re.compile(r'\d\d\.\d\d\.(\d\d\d\d|\d\d)')
 
-def status_log():
-    queryset = petitions.find({})
-    for pet in queryset:
-        date = pet.get('date')
-        status = pet.get('status')
-        status_creator = pet.get('status_creator')
-        creator = status_creator if status_creator is not None else pet.get('user_id')
-        if status_creator is None:
-            petitions.update_one(
-                {'_id': pet.get('_id')},
-                {'$set': {'status_log': {status: [creator, date]}, 'status_creator': pet.get('user_id')}}
-            )
-        else:
-            petitions.update_one(
-                {'_id': pet.get('_id')},
-                {'$set': {'status_log': {status: [creator, date]}}}
-            )
-
-status_log()
+for group in GROUP_NAMES:
+    date = date_reg.search(group)
+    day, month, year = date.group().split('.')
+    year = f'20{year}' if len(year) == 2 else year
+    date = f'{day}.{month}.{year}'
+    print(date)
