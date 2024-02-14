6d7cd4b9afc0dd718838d5004d3337400b3d86c0
Alexey S <huji@mail.ru>
2024-02-14 18:08:22 +0500

    fix

--- utils/test.py
+++ utils/test.py
@@ -75,25 +75,3 @@
 #     print(count)
 
 # pop_aos()
-
-"""Пример работы с чатом через gigachain"""
-from langchain.schema import HumanMessage, SystemMessage
-from langchain.chat_models.gigachat import GigaChat
-
-# Авторизация в сервисе GigaChat
-chat = GigaChat(credentials='Yzk1ODc3NTEtMGRlMy00OWUxLTkzMzEtMzc1ZjRjN2UzYzFkOmU3MTQwNWY2LWNiZjYtNGNlNi1hYTc5LWUyMDM5YjlhODIxZA==', verify_ssl_certs=False)
-
-messages = [
-    SystemMessage(
-        content="Ты бот-механик, который помогает пользователю решить его проблемы с технологическим оборудованием."
-    )
-]
-
-while(True):
-    # Ввод пользователя
-    user_input = input("User: ")
-    messages.append(HumanMessage(content=user_input))
-    res = chat(messages)
-    messages.append(res)
-    # Ответ модели
-    print("Bot: ", res.content)
